dbd1b93506960a9f5745cd3038d0546c08badfee
sukhman31 <labana.sukhman@gmail.com>
2024-08-10 17:54:10 +0530

    renaming module and creating samples folder

---
+++ samples/Naive_Bayes.py
@@ -0,0 +1,26 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from skhmn_learn.feature_extraction import count_vectorizer
+from skhmn_learn.naive_bayes import nb
+
+vec = count_vectorizer.CountVectorizer()
+nb = nb.NaiveBayes()
+
+corpus = ["Chinese Beijing Chinese","Chinese Chinese Shanghai","Chinese Macao","Tokyo Japan Chinese"]
+
+vec.fit(corpus)
+
+print(vec.countOfWords)
+print(vec.dictionary)
+print(vec.fitMatrix)
+
+data = vec.fitMatrix
+label = [1,1,1,0]
+
+nb.train(data, label)
+
+test_data = vec.transform(["Chinese Chinese Chinese Tokyo Japan"])
+print("test_data", test_data)
+print(nb.calculate_prob(test_data[0]))
