1a832b3c75a37ea4dbadba4b09c5ca7fe3dc0ac7
sukhman31 <labana.sukhman@gmail.com>
2024-08-18 21:25:28 +0530

    training logic for decision trees

--- samples/Decision_Trees.py
+++ samples/Decision_Trees.py
@@ -9,12 +9,32 @@ from skhmn_learn.decision_trees.trees import Impurity
 
 dt = DecisionTrees()
 
-print(dt.gini_impurity(np.array([1,1,0,1,0])))
-print(dt.gini_impurity(np.array([1,1,0,1,0,0])))
-print(dt.gini_impurity(np.array([1,1,1,1])))
+# print(dt.gini_impurity(np.array([1,1,0,1,0])))
+# print(dt.gini_impurity(np.array([1,1,0,1,0,0])))
+# print(dt.gini_impurity(np.array([1,1,1,1])))
 
-print(dt.entropy(np.array([1,1,0,1,0])))
-print(dt.entropy(np.array([1,1,0,1,0,0])))
-print(dt.entropy(np.array([1,1,1,1])))
+# print(dt.entropy(np.array([1,1,0,1,0])))
+# print(dt.entropy(np.array([1,1,0,1,0,0])))
+# print(dt.entropy(np.array([1,1,1,1])))
 
-print(dt.weighted_impurity([[1, 0, 1], [0, 1]], Impurity.ENTROPY))
+# print(dt.weighted_impurity([[1, 0, 1], [0, 1]], Impurity.ENTROPY))
+
+X_train = np.array([['tech', 'professional'],
+['fashion', 'student'],
+['fashion', 'professional'],
+['sports', 'student'],
+['tech', 'student'],
+['tech', 'retired'],
+['sports', 'professional']])
+y_train = np.array([1, 0, 0, 0, 1, 0, 1])
+
+dt.train_tree(X_train, y_train)
+print("------------")
+print(dt.parent.left_child.training_data)
+print(dt.parent.right_child.training_data)
+print("------------")
+print(dt.parent.left_child.left_child.training_data)
+print(dt.parent.left_child.right_child.training_data)
+print("------------")
+print(dt.parent.right_child.left_child)
+print(dt.parent.right_child.right_child)
--- skhmn_learn/decision_trees/trees.py
+++ skhmn_learn/decision_trees/trees.py
@@ -12,24 +12,53 @@ class Node:
         self.is_leaf_node = False
         self.left_child = None
         self.right_child = None
+        self.training_data = None
+        self.trainig_class = None
+
+    def __init__(self, is_leaf_node, left_child, right_child, training_data, training_class):
+        self.is_leaf_node = is_leaf_node
+        self.left_child = left_child
+        self.right_child = right_child
+        self.training_data = training_data
+        self.trainig_class = training_class
 
 class DecisionTrees:
     
     def __init__(self):
-        None
+        self.parent = None
 
     def train_tree(self, training_data: np.ndarray, training_class: np.ndarray):
-        parent = self.recursive_build_tree(training_data, training_class, Node())
+        self.parent = self.recurive_build_tree(training_data, training_class)
     
-    def recurive_build_tree(self, training_data: np.ndarray, training_class: np.ndarray, root: Node):
-        best_split = self.get_best_split()
+    def recurive_build_tree(self, training_data: np.ndarray, training_class: np.ndarray):
+        if len(np.unique(training_class)) == 1:
+            return Node(True, None, None, training_data, training_class)
+        left_split, right_split = self.get_best_split(training_data, training_class)
+        return Node(False, self.recurive_build_tree(left_split[0], left_split[1]), self.recurive_build_tree(right_split[0], right_split[1]), training_data, training_class)
     
     def get_best_split(self, training_data: np.ndarray, training_class: np.ndarray):
-        for index in range(training_data.shape[0]):
-            split = self.get_split(training_data, training_class, index)
+        min_impurity = float('inf')
+        best_index = None
+        best_entry = None
+        for index in range(training_data.shape[1]):
+            unique_entries = np.sort(np.unique(training_data[:, index]))
+            for entry in unique_entries:
+                left_split, right_split = self.get_split(training_data, training_class, index, entry)
+                imp = self.weighted_impurity([left_split[1], right_split[1]], Impurity.GINI)
+                if imp < min_impurity:
+                    min_impurity = imp
+                    best_index = index
+                    best_entry = entry
+        return self.get_split(training_data, training_class, best_index, best_entry)
     
-    def get_split(self, training_data: np.ndarray, training_class: np.ndarray, index: int):
-        
+    def get_split(self, training_data: np.ndarray, training_class: np.ndarray, index: int, entry):
+        data_index = training_data[:, index]
+        if training_data[:, index].dtype.kind in ['i', 'f']:
+            mask = data_index >= entry
+        else:
+            mask = data_index == entry
+        return [training_data[~mask, :], training_class[~mask]], [training_data[mask, :], training_class[mask]]
+
 
     def gini_impurity(self, labels: np.ndarray):
         impurity = 1.0
